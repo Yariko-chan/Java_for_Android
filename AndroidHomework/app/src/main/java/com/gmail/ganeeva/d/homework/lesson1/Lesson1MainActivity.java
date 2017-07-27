849bca0c78b3e3a518695f8e2909e396e50a23c4
Yariko-chan <lililala1991@gmail.com>
2017-07-27 22:54:49 +0300

    Homework 1

--- AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson1/Lesson1MainActivity.java
+++ AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson1/Lesson1MainActivity.java
@@ -2,14 +2,53 @@ package com.gmail.ganeeva.d.homework.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.gmail.ganeeva.d.homework.R;
 
-public class Lesson1MainActivity extends AppCompatActivity {
+public class Lesson1MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView upperTv;
+    private TextView downTV;
+    private Button exchButton;
+
+    private View.OnClickListener exchanger = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String upper = upperTv.getText().toString();
+            String down = downTV.getText().toString();
+            upperTv.setText(down);
+            downTV.setText(upper);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson1_main);
+
+        upperTv = (TextView) findViewById(R.id.upperTextview);
+        downTV = (TextView) findViewById(R.id.downTextview);
+        exchButton = (Button) findViewById(R.id.exchangeButton);
+        upperTv.setOnClickListener(this);
+        downTV.setOnClickListener(exchanger);
+        exchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String upper = upperTv.getText().toString();
+                String down = downTV.getText().toString();
+                upperTv.setText(down);
+                downTV.setText(upper);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        String upper = upperTv.getText().toString();
+        String down = downTV.getText().toString();
+        upperTv.setText(down);
+        downTV.setText(upper);
     }
 }
