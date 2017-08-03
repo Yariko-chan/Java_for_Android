e48da39ad4a1c36036d894aca96869f01bf9bcb5
D.Ganeeva <lililala1991@gmail.com>
2017-08-03 19:08:31 +0300

    Homework 4-2 (animated owl)

--- AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson4/Lesson4ClockActivity.java
+++ AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson4/Lesson4ClockActivity.java
@@ -1,7 +1,9 @@
 package com.gmail.ganeeva.d.homework.lesson4;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.gmail.ganeeva.d.homework.R;
 
@@ -11,5 +13,9 @@ public class Lesson4ClockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson4_clock);
+
+        ImageView owlImage = (ImageView) findViewById(R.id.owl_image);
+        AnimationDrawable owlAnimation = (AnimationDrawable) owlImage.getBackground();
+        owlAnimation.start();
     }
 }
