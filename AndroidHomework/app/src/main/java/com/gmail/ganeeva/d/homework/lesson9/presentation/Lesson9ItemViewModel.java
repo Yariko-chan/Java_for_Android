c37b8463007f0914351056702936c3c7f6ff7f39
D.Ganeeva <lililala1991@gmail.com>
2017-08-17 16:36:36 +0300

    for merging

--- AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson9/presentation/Lesson9ItemViewModel.java
+++ AndroidHomework/app/src/main/java/com/gmail/ganeeva/d/homework/lesson9/presentation/Lesson9ItemViewModel.java
@@ -1,9 +1,16 @@
 package com.gmail.ganeeva.d.homework.lesson9.presentation;
 
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 /**
  * Created by Diana on 16.08.2017 at 17:27.
@@ -17,6 +24,18 @@ public class Lesson9ItemViewModel {
     public static void loadImage(ImageView view, String url) {
         Glide.with(view.getContext())
             .load(url)
+            .listener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                    holder.progressBar.setVisibility(View.GONE);
+                    holder.imageView.setVisibility(View.VISIBLE);
+                    return false;
+                }
             .into(view);
     }
 }
